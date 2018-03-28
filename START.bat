02905fb3eb10cd75a701af93707925982b696a4d
Ningaro <baev.dima.77@gmail.com>
2018-03-28 15:49:58 +0300

    Fix

--- START.bat
+++ START.bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --app="https://tennesine.github.io/Chim-Table/" 
-=======
->>>>>>> e14e46f49ab3b0620dd7085f055023960d69df23
-pause
+start chrome --app="file:///%~dp0index.html"
