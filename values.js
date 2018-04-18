857c826cd64a2abb346aaaab9cd88bb9835c043a
Ningaro <baev.dima.77@gmail.com>
2018-04-18 19:26:39 +0300

    Smart modal

---
+++ values.js
@@ -0,0 +1,17 @@
+const values = {
+  "hydrogen":{
+    "label":"H - Водород",
+    "desc":"text1",
+    "history":"Text2",
+    "nature":"text3",
+    "use":"text4",
+    "img":"img5"
+  },
+  "helium":{
+    "desc":"text6",
+    "history":"Text7",
+    "nature":"text8",
+    "use":"text9",
+    "img":"img10"
+  }
+}
